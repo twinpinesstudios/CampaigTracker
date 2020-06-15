e736b1b14cb6694b6157faac27e896b1a262b34b
Will Claringbold <will.claringbold@cubic-interactive.com>
2020-06-16 00:02:40 +0100

    Added unit tests and faction controller

---
+++ CampaignTracker.Api.Controllers.Tests/UnitTest1.cs
@@ -0,0 +1,69 @@
+using Abstractions;
+using Factions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampaignTracker.Api.Controllers.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Mock<IRepositoryProvider> mockRepositoryProvider;
+        private Mock<IRepository<Faction>> mockFactionRepository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockRepositoryProvider = new Mock<IRepositoryProvider>(MockBehavior.Strict);
+            mockFactionRepository = new Mock<IRepository<Faction>>(MockBehavior.Strict);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            mockRepositoryProvider.Verify();
+            mockFactionRepository.Verify();
+        }
+
+        [TestMethod]
+        public async Task GetShouldReturnAQueryableListOfFactions()
+        {
+            var factions = new List<Faction>
+            {
+                new Faction{Name = "faction1"},
+                new Faction{Name = "faction2"},
+                new Faction{Name = "faction3"},
+                new Faction{Name = "faction4"},
+            };
+
+            mockRepositoryProvider
+                .Setup(x => x.RepositoryFor<Faction>())
+                .Returns(mockFactionRepository.Object)
+                .Verifiable();
+
+            mockFactionRepository
+                .Setup(x => x.CreateQuery())
+                .Returns(factions.AsQueryable())
+                .Verifiable();
+
+            var subject = new FactionController(mockRepositoryProvider.Object);
+
+            var result = await subject.Get();
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okObjectResult = result as OkObjectResult;
+            Assert.IsNotNull(okObjectResult);
+            var results = okObjectResult.Value as IQueryable<Faction>;
+            Assert.IsNotNull(results);
+            Assert.AreEqual(4, results.Count());
+            Assert.IsTrue(results.Any(x => x.Name == "faction1"));
+            Assert.IsTrue(results.Any(x => x.Name == "faction2"));
+            Assert.IsTrue(results.Any(x => x.Name == "faction3"));
+            Assert.IsTrue(results.Any(x => x.Name == "faction4"));
+        }
+    }
+}
---
+++ CampaignTracker.Api.Controllers/FactionController.cs
@@ -0,0 +1,31 @@
+﻿using Abstractions;
+using Factions;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CampaignTracker.Api.Controllers
+{
+    [Microsoft.AspNetCore.Authorization.Authorize]
+    public class FactionController
+        : ODataController
+    {
+        private readonly IRepositoryProvider repositoryProvider;
+
+        public FactionController(IRepositoryProvider repositoryProvider)
+        {
+            this.repositoryProvider = repositoryProvider;
+        }
+        
+        [HttpGet]
+        [EnableQuery]
+        [ODataRoute("Factions")]
+        public async Task<IActionResult> Get()
+        {
+            var results = this.repositoryProvider.RepositoryFor<Faction>().CreateQuery();
+
+            return this.Ok(results);
+        }
+    }
+}
